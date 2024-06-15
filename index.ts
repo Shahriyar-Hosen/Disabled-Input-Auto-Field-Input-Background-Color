bc189180eb06275d123ea4b8494bf11825a0a0b2
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2024-06-15 11:56:05 +0600

    done

---
+++ index.ts
@@ -0,0 +1,15 @@
+/* 
+input field a suggestion ar kisu select korle browser ar default akti color add kore seita modify korte ai code ti likhun
+
+1. Add the following CSS to your page:
+*/
+
+// input:-webkit-autofill,
+// input:-webkit-autofill:hover,
+// input:-webkit-autofill:focus,
+// input:-webkit-autofill:active {
+//   -webkit-background-clip: text;
+//   -webkit-text-fill-color: #424959; /* text-color */
+//   transition: background-color 5000s ease-in-out 0s;
+//   box-shadow: inset 0 0 50px 50px #fff; /*  bg color */
+// }
